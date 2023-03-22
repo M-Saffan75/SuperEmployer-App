7bd67a3af32eb0e09678fda8505e95c3834bfca1
M.Saffan Khan <92519898+M-Saffan75@users.noreply.github.com>
2023-03-22 05:31:40 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import DrawerHere from './Screens/DrawerHere'
+import Navigator from './AppNavigator/Navigator'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <DrawerHere />
+      {/* <Navigator /> */}
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
